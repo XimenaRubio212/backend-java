ce75378b8ccfff430e1a2e516c60e942f9d2f528
ximena <ximenarubio121@gmail.com>
2026-02-17 01:59:55 -0500

    Se logro hacer que aparezca el HTTP 405 que el servidor esta funcionado

--- src/main/java/com/tastytap/servlets/LoginServlet.java
+++ src/main/java/com/tastytap/servlets/LoginServlet.java
@@ -4,14 +4,22 @@ import com.tastytap.dao.UsuarioDao;
 import com.tastytap.modelo.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
 
 @WebServlet("/api/login")
 public class LoginServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        // --- CABECERAS CORS ---
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
         response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
 
         String nombre = request.getParameter("nombre_login");
@@ -26,5 +34,13 @@ public class LoginServlet extends HttpServlet {
         } else {
             out.print("{\"status\":\"error\", \"message\":\"Usuario o contraseña incorrectos.\"}");
         }
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
